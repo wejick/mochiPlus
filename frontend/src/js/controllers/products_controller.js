5b9dcbaeb4d5aa922e3379b7f399ada4e886ff98
giovani.gian <wejick@gmail.com>
2016-07-22 22:11:50 +0700

    get data from server

--- frontend/src/js/controllers/products_controller.js
+++ frontend/src/js/controllers/products_controller.js
@@ -1,48 +1,22 @@
 angular.module('Up+.controllers.Products', [])
 
-.controller('ProductsController', function($scope){
-  $scope.data = [
-    {
-      title :"Kacang goreng",
-      price : 3000,
-      shop  : "Surabaya cell",
-      location : "Yogyakarta",
-      image : "images/ijo.png"
-    },
-    {
-      title : "Kacang panjang",
-      price : 5000,
-      shop  : "Mentari pagi",
-      location : "Kediri",
-      image : "images/ijo.png"
-    },
-    {
-      title : "Kacang Ijo",
-      price : 5000,
-      shop  : "Sahabat pagi",
-      location : "Kediri",
-      image : "images/ijo.png"
-    },
-    {
-      title : "Kacang kelici",
-      price : 4000,
-      shop  : "Mentari pagi",
-      location : "Kediri",
-      image : "images/ijo.png"
-    },
-    {
-      title : "Kacang atom",
-      price : 15000,
-      shop  : "Cahaya Sahabat",
-      location : "Blitar",
-      image : "images/ijo.png"
-    },
-    {
-      title : "Kacang Merah",
-      price : 51000,
-      shop  : "Mentari Bersemi",
-      location : "Kediri",
-      image : "images/ijo.png"
-    }
-  ]
+.controller('ProductsController', function($scope,$http) {
+
+  //get data product list from server
+    $http({
+      method  : 'GET',
+      url     : "https://hackathon.tokopedia.com/api/product/list",
+      data    : "",
+      headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }).success(function(res) {
+      $scope.data = res.data;
+    });
+    
+  //get pending data
+  $scope.pendingUpload = "-";
+  if($scope.pendingUpload != "-") {
+    
+  } else {
+    
+  }
 });
